f113efbb705cda98de9b02e64244a426a75c499b
gkorolkevich <python1jponce@student.21-school.ru>
2023-08-29 19:30:53 +0300

    rework PropertyFactory to kt style

--- client-kotlin/src/main/java/com/netflix/conductor/client/kotlin/config/PropertyFactory.kt
+++ client-kotlin/src/main/java/com/netflix/conductor/client/kotlin/config/PropertyFactory.kt
@@ -10,82 +10,80 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package com.netflix.conductor.client.kotlin.config;
+package com.netflix.conductor.client.kotlin.config
 
-import com.netflix.config.DynamicProperty;
+import com.netflix.config.DynamicProperty
+import java.util.concurrent.ConcurrentHashMap
 
-import java.util.concurrent.ConcurrentHashMap;
+/** Used to configure the Conductor workers using properties.  */
+class PropertyFactory private constructor(prefix: String, propName: String, workerName: String) {
+    private val global: DynamicProperty
+    private val local: DynamicProperty
 
-/** Used to configure the Conductor workers using properties. */
-public class PropertyFactory {
-
-    private final DynamicProperty global;
-    private final DynamicProperty local;
-
-    private static final String PROPERTY_PREFIX = "conductor.worker";
-
-    private static final ConcurrentHashMap<String, PropertyFactory> PROPERTY_FACTORY_MAP =
-            new ConcurrentHashMap<>();
-
-    private PropertyFactory(String prefix, String propName, String workerName) {
-        this.global = DynamicProperty.getInstance(prefix + "." + propName);
-        this.local = DynamicProperty.getInstance(prefix + "." + workerName + "." + propName);
+    init {
+        global = DynamicProperty.getInstance("$prefix.$propName")
+        local = DynamicProperty.getInstance("$prefix.$workerName.$propName")
     }
 
     /**
      * @param defaultValue Default Value
      * @return Returns the value as integer. If not value is set (either global or worker specific),
-     *     then returns the default value.
+     * then returns the default value.
      */
-    public Integer getInteger(int defaultValue) {
-        Integer value = local.getInteger();
-        if (value == null) {
-            value = global.getInteger(defaultValue);
-        }
-        return value;
-    }
+    fun getInteger(): Int? =
+        local.integer ?: global.integer
 
     /**
      * @param defaultValue Default Value
      * @return Returns the value as String. If not value is set (either global or worker specific),
-     *     then returns the default value.
+     * then returns the default value.
      */
-    public String getString(String defaultValue) {
-        String value = local.getString();
-        if (value == null) {
-            value = global.getString(defaultValue);
-        }
-        return value;
-    }
+    fun getString(): String? =
+        local.string ?: global.string
 
     /**
      * @param defaultValue Default Value
      * @return Returns the value as Boolean. If not value is set (either global or worker specific),
-     *     then returns the default value.
+     * then returns the default value.
      */
-    public Boolean getBoolean(Boolean defaultValue) {
-        Boolean value = local.getBoolean();
-        if (value == null) {
-            value = global.getBoolean(defaultValue);
+    fun getBoolean(): Boolean? =
+        local.boolean ?: global.boolean
+
+    companion object {
+        private const val PROPERTY_PREFIX = "conductor.worker"
+        private val PROPERTY_FACTORY_MAP = ConcurrentHashMap<String, PropertyFactory>()
+        @JvmStatic
+        fun getInteger(workerName: String, property: String, defaultValue: Int? = null): Int? {
+            return getPropertyFactory(workerName, property).getInteger() ?: defaultValue
         }
-        return value;
-    }
 
-    public static Integer getInteger(String workerName, String property, Integer defaultValue) {
-        return getPropertyFactory(workerName, property).getInteger(defaultValue);
-    }
+        fun getInteger(workerName: String, property: String, defaultValue: Int): Int {
+            return getPropertyFactory(workerName, property).getInteger() ?: defaultValue
+        }
 
-    public static Boolean getBoolean(String workerName, String property, Boolean defaultValue) {
-        return getPropertyFactory(workerName, property).getBoolean(defaultValue);
-    }
+        @JvmStatic
+        fun getBoolean(workerName: String, property: String, defaultValue: Boolean? = null): Boolean? {
+            return getPropertyFactory(workerName, property).getBoolean() ?: defaultValue
+        }
 
-    public static String getString(String workerName, String property, String defaultValue) {
-        return getPropertyFactory(workerName, property).getString(defaultValue);
-    }
+        fun getBoolean(workerName: String, property: String, defaultValue: Boolean): Boolean {
+            return getPropertyFactory(workerName, property).getBoolean() ?: defaultValue
+        }
 
-    private static PropertyFactory getPropertyFactory(String workerName, String property) {
-        String key = property + "." + workerName;
-        return PROPERTY_FACTORY_MAP.computeIfAbsent(
-                key, t -> new PropertyFactory(PROPERTY_PREFIX, property, workerName));
+        @JvmStatic
+        fun getString(workerName: String, property: String, defaultValue: String? = null): String? {
+            return getPropertyFactory(workerName, property).getString() ?: defaultValue
+        }
+
+        fun getString(workerName: String, property: String, defaultValue: String): String {
+            return getPropertyFactory(workerName, property).getString() ?: defaultValue
+        }
+
+        private fun getPropertyFactory(workerName: String, property: String): PropertyFactory {
+            val key = "$property.$workerName"
+            return PROPERTY_FACTORY_MAP.computeIfAbsent(
+                key
+            ) { PropertyFactory(PROPERTY_PREFIX, property, workerName) }
+        }
     }
 }
--- client-kotlin/src/test/java/com/netflix/conductor/client/kotlin/config/TestPropertyFactory.kt
+++ client-kotlin/src/test/java/com/netflix/conductor/client/kotlin/config/TestPropertyFactory.kt
@@ -1,55 +1,61 @@
-package com.netflix.conductor.client.kotlin.config;
+package com.netflix.conductor.client.kotlin.config
 
-import com.netflix.conductor.client.kotlin.worker.Worker;
-import com.netflix.conductor.common.metadata.tasks.TaskResult;
-import org.junit.Test;
+import com.netflix.conductor.client.kotlin.config.PropertyFactory.Companion.getBoolean
+import com.netflix.conductor.client.kotlin.config.PropertyFactory.Companion.getInteger
+import com.netflix.conductor.client.kotlin.config.PropertyFactory.Companion.getString
+import com.netflix.conductor.client.kotlin.worker.Worker.Companion.create
+import com.netflix.conductor.common.metadata.tasks.TaskResult
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
-import static org.junit.Assert.*;
-
-public class TestPropertyFactory {
-
+class TestPropertyFactory {
     @Test
-    public void testIdentity() {
-        Worker worker = Worker.create("Test2", TaskResult::new);
-        assertNotNull(worker.getIdentity());
-        boolean paused = worker.paused();
-        assertFalse("Paused? " + paused, paused);
+    fun testIdentity() {
+        val worker = create("Test2") { task -> TaskResult(task) }
+        assertNotNull(worker.identity)
+        val paused = worker.paused()
+        assertFalse(paused, "Paused? $paused")
     }
 
     @Test
-    public void test() {
-
-        int val = PropertyFactory.getInteger("workerB", "pollingInterval", 100);
-        assertEquals("got: " + val, 2, val);
+    fun test() {
+        val property = getInteger("workerB", "pollingInterval", 100)
+        assertEquals( 2, property.toLong(), "got: $property",)
         assertEquals(
-                100, PropertyFactory.getInteger("workerB", "propWithoutValue", 100).intValue());
-
+            100, getInteger("workerB", "propWithoutValue", 100).toLong()
+        )
         assertFalse(
-                PropertyFactory.getBoolean(
-                        "workerB", "paused", true)); // Global value set to 'false'
+            getBoolean(
+                "workerB", "paused", true
+            )
+        ) // Global value set to 'false'
         assertTrue(
-                PropertyFactory.getBoolean(
-                        "workerA", "paused", false)); // WorkerA value set to 'true'
-
+            getBoolean(
+                "workerA", "paused", false
+            )
+        ) // WorkerA value set to 'true'
         assertEquals(
-                42,
-                PropertyFactory.getInteger("workerA", "batchSize", 42)
-                        .intValue()); // No global value set, so will return the default value
+            42,
+            getInteger("workerA", "batchSize", 42).toLong()
+        ) // No global value set, so will return the default value
         // supplied
         assertEquals(
-                84,
-                PropertyFactory.getInteger("workerB", "batchSize", 42)
-                        .intValue()); // WorkerB's value set to 84
-
-        assertEquals("domainA", PropertyFactory.getString("workerA", "domain", null));
-        assertEquals("domainB", PropertyFactory.getString("workerB", "domain", null));
-        assertNull(PropertyFactory.getString("workerC", "domain", null)); // Non Existent
+            84,
+            getInteger("workerB", "batchSize", 42).toLong()
+        ) // WorkerB's value set to 84
+        assertEquals("domainA", getString("workerA", "domain"))
+        assertEquals("domainB", getString("workerB", "domain"))
+        assertNull(getString("workerC", "domain")) // Non Existent
     }
 
     @Test
-    public void testProperty() {
-        Worker worker = Worker.create("Test", TaskResult::new);
-        boolean paused = worker.paused();
-        assertTrue("Paused? " + paused, paused);
+    fun testProperty() {
+        val worker = create("Test") { task -> TaskResult(task) }
+        val paused = worker.paused()
+        assertTrue(paused, "Paused? $paused")
     }
 }
